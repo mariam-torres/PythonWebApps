00438273e909ef57463be68da1f148a00a13e370
Mariam Torres <torr1658@bears.unco.edu>
2024-09-12 09:24:54 -0600

    Lesson 4 Practice (2)

--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,6 +1,20 @@
 from django.views.generic import TemplateView
 
-
 class HulkView(TemplateView):
     template_name = 'hero.html'
 
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Hulk',
+            'id': 'Bruce Banner',
+            'image': '/static/images/hulk.jpg'
+        } 
+class IronManView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'IronMan',
+            'id': 'Tony Stark',
+            'image': '/static/images/iron_man.jpg'
+        } 
