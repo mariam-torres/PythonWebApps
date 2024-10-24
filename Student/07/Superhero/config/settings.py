d31ec355d7ce2002e04ed57e60c85e3a0b1e72d1
Mariam Torres <torr1658@bears.unco.edu>
2024-10-24 17:05:41 -0600

    crispy

--- Student/07/Superhero/config/settings.py
+++ Student/07/Superhero/config/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hero',
-    'crispy_forms',
-    'crispy_bootstrap5',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
