b7076ad881c8ce60aee9405d312b0ff871768c7c
Mariam Torres <torr1658@bears.unco.edu>
2024-10-24 20:40:57 -0600

    setting3

--- Student/07/Superhero/config/settings.py
+++ Student/07/Superhero/config/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SuperProfiles',
+    'hero',
     # Styling
     "crispy_forms",
     "crispy_bootstrap5",
