a53ea655a4d1d7fa8c0c48844f6ee59b19df9cbb
Mariam Torres <torr1658@bears.unco.edu>
2024-11-04 09:22:27 -0700

    Project08

---
+++ Student/08/Superhero/hero/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+class Reporter(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+
+    realName = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('reporter_detail', args=[str(self.id)])
+
+    @property
+    def name(self):
+        return self.user.first_name + ' ' + self.user.last_name
+
+    @property
+    def heroes(self):
+        return Hero.objects.filter(author=self)
+
+    @staticmethod
+    def get_me(user):
+        return Reporter.objects.get_or_create(user=user)[0]
+
+class Hero(models.Model):
+
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE, editable=False)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    realName = models.CharField(max_length=200)
+    strength1 = models.CharField(max_length=200)
+    strength2 = models.CharField(max_length=200)
+    strength3 = models.CharField(max_length=200)
+    weakness1 = models.CharField(max_length=200)
+    weakness2 = models.CharField(max_length=200)
+    weakness3 = models.CharField(max_length=200)
+    imagePath = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.heropk}. {self.title} - {self.realName}"
